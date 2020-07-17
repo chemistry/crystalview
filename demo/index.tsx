81d8f31a28f2ac35876dbb72116a23f22ac5b354
Volodymyr Vreshch <vreshch@gmail.com>
2020-07-17 03:33:10 +0300

    Empty state

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,8 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { CrystalView } from '../src';
+import { CrystalView, CrystalViewState } from '../src';
+
+const Viewer = () => {
+    const [appState, setAppState] = React.useState(
+        () => CrystalViewState.createEmpty(),
+    );
+
+    return (<div style={{ width: '600px' }}>
+        <CrystalView appState={appState} onChange={setAppState} />
+    </div>)
+}
+
 
 ReactDOM.render(
-    <CrystalView />,
+    <Viewer />,
     document.getElementById("app"),
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,24 @@
 import * as React from 'react';
 
-export const CrystalView = () => {
-    return (<h1>Crystal View</h1>)
+export const CrystalView = ({ appState, onChange }) => {
+    const ref = React.useRef();
+    return (
+        <div style={{"background-color": '#2b303b', "dispaly": "block"}} className="crystal-view">
+            <style dangerouslySetInnerHTML={{__html: `
+                .crystal-view { }
+                .crystal-view:after {
+                    content: "";
+                    display: block;
+                    padding-bottom: 75%;
+                }
+            `}}/>
+            <canvas ref={ref}/>
+        </div>
+    )
+}
+
+export const CrystalViewState = {
+    createEmpty: () => {
+        return {}
+    }
 }
